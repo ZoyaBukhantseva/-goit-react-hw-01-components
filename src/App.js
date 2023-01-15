983b3547e63c28f31f426d8f80a4639cab389df1
ZoyaBukhantseva <zoyapashchenko87@gmail.com>
2023-01-16 01:22:46 +0300

    update hw

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import transactions from '..//src/data/transactions.json';
 const App = () => (
   <>
     <Profile
-      name={user.name}
+      name={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
